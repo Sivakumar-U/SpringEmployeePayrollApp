c54dfc8bf183ed3536283d53fad18a734cf8d3bf
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-20 13:01:37 +0530

    Added: Code to introduce DTO and model to employee payroll app

--- src/main/java/com/bridgelabz/springemployeepayrollapplication/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/springemployeepayrollapplication/controller/EmployeePayrollController.java
@@ -12,34 +12,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.springemployeepayrollapplication.dto.EmployeePayrollDTO;
+import com.bridgelabz.springemployeepayrollapplication.dto.ResponseDTO;
+import com.bridgelabz.springemployeepayrollapplication.model.EmployeePayrollData;
 
 @RestController
 @RequestMapping("/employeepayroll")
 public class EmployeePayrollController {
 
-	@GetMapping("/get")
-	public ResponseEntity<String> getEmployeePayrollData() {
-		return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
+	@RequestMapping(value = { "", "/", "get" })
+	public ResponseEntity<ResponseDTO> getEmployeePayrollData() {
+		EmployeePayrollData payrollData = null;
+		payrollData = new EmployeePayrollData(1, new EmployeePayrollDTO("Sivakumar", 500000));
+		ResponseDTO responseDTO = new ResponseDTO("Get Call Success", payrollData);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
 	@GetMapping("/get/{empId}")
-	public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId") int empId) {
-		return new ResponseEntity<String>("Get Call Success for id:" + empId, HttpStatus.OK);
+	public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId) {
+		EmployeePayrollData payrollData = null;
+		payrollData = new EmployeePayrollData(2, new EmployeePayrollDTO("Ramana", 450000));
+		ResponseDTO responseDTO = new ResponseDTO("Get Call Success for id:", payrollData);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDto) {
-		return new ResponseEntity<String>("Created Employee payroll data for:" + employeePayrollDto, HttpStatus.OK);
+	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+		EmployeePayrollData payrollData = null;
+		payrollData = new EmployeePayrollData(3, employeePayrollDTO);
+		ResponseDTO responseDTO = new ResponseDTO("Created Employee payroll data for:", payrollData);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
 	@PutMapping("/update")
-	public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDto) {
-		return new ResponseEntity<String>("Updated Employee payroll Data for: " + employeePayrollDto, HttpStatus.OK);
+	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+		EmployeePayrollData payrollData = null;
+		payrollData = new EmployeePayrollData(3, employeePayrollDTO);
+		ResponseDTO responseDTO = new ResponseDTO("Updated Employee payroll Data for: ", payrollData);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete/{empId}")
-	public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
-		return new ResponseEntity<String>("Delete Call Success for id: " + empId, HttpStatus.OK);
+	public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
+		ResponseDTO responseDTO = new ResponseDTO("Delete Call Success for id: ", "empId " + empId);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/bridgelabz/springemployeepayrollapplication/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/springemployeepayrollapplication/dto/EmployeePayrollDTO.java
@@ -2,4 +2,18 @@ package com.bridgelabz.springemployeepayrollapplication.dto;
 
 public class EmployeePayrollDTO {
 
+	public String name;
+	public long salary;
+
+	public EmployeePayrollDTO(String name, long salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
+	}
+
 }
